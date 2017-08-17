0ce1ccfd2c42bc00cec9a3be699c1d0dee569548
Seguleh <aezhco@gmail.com>
2017-08-17 15:07:46 -0500

    Adding comments to code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,7 @@ var d=new Date(), currM=d.getMonth(), currY=d.getFullYear();
 
 window.onload = function (){
 
+  //Set month and year
   setDate('calendar-my', month[d.getMonth()]+' '+currY);
   makeCalendar();
 };
@@ -19,6 +20,7 @@ function changeDate(id) {
   curr = document.getElementById('calendar-my').innerHTML.trim().split(" ")
   currM = month.indexOf(curr[0]);
   if(id == 'next'){
+    //Increment month or check if its December and reset with year increment
     if (currM == 11){
       currM=0;
       currY++;
@@ -26,10 +28,13 @@ function changeDate(id) {
       currM++;
     }
     setDate('calendar-my', month[currM]+' '+currY);
+    //Remove old calendar
     $("#calendar").remove();
+    //Build tbody of new calendar and apply Fade effect
     $('<tbody id="calendar"></tbody>').fadeIn(1000).insertAfter("thead");
     makeCalendar();
   }else{
+    //Decrement month or check if its January and reset with year decrement
     if (currM == 0){
       currM=11;
       currY--;
@@ -37,7 +42,9 @@ function changeDate(id) {
       currM--;
     }
     setDate('calendar-my', month[currM]+' '+currY);
+    //Remove old calendar
     $("#calendar").remove();
+    //Build tbody of new calendar and apply Fade effect
     $('<tbody id="calendar"></tbody>').fadeIn(1000).insertAfter("thead");
     makeCalendar();
   }
@@ -46,30 +53,33 @@ function changeDate(id) {
 function makeCalendar () {
 
   var i = m = 1;
-
   // Get the first day of the week in set month and year: 0=Su, 1=Mo, 2=Tu, etc
   var firstDay = new Date(currY, currM, 1).getDay();
-
   // Get number of days in a month
   var daysMonth = new Date(currY, currM, 0).getDate();
   //Creating the days in a month, taking into account the firstDay of the week
   loop1:
    for (n = 1; n < 6; n++) {
+     //Creating new tr's with row id
      r = document.getElementById('calendar').appendChild(document.createElement("tr"));
      r.setAttribute("id", "row"+n);
     for (j = 0; j < 7; j++) {
+      //Break loop if daysMonth is achieved
       if (m > daysMonth) {
         break loop1;
       }
       if (i > firstDay) {
+        //Create td with day number
         td = document.createElement("td");
         td.appendChild(document.createTextNode(m.toString()));
+        //Highlight current day
         if (d.getMonth() == currM && m == d.getDate()){
           td.setAttribute("class", "currDay")
         }
         document.getElementById('row'+n).appendChild(td);
         m++;
       }else {
+        //Create td without number to maintain aspect of calendar
         document.getElementById('row'+n).appendChild(document.createElement("td"));
       }
       i++;
