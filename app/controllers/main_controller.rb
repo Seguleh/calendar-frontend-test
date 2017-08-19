9d2cd26425ea3bbb948f267719bafc8a5f2d68ff
Seguleh <aezhco@gmail.com>
2017-08-19 17:59:10 -0500

    More Heroku fixing

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,4 @@
 class MainController < ApplicationController
   def index
-    @main = Main.all
-    main = @main.to_json
-    respond_to do |format|
-    format.json {render :json => main, :callback => params[:callback]}
   end
 end
